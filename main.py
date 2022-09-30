cd901e38b0472cffc60268034651cb928eb632b6
zaboulia <zaboulia@uno.edu>
2022-09-30 15:52:23 +0200

    testing seed

--- main.py
+++ main.py
@@ -9,15 +9,20 @@ def generateEntropy():
     return rng,hashe
 
 def generateSeed():
-    rng, hashe = generateEntropy()
-    result = rng + hashe[:4]
-    print(result)
-    print("taille : ",len(result))
-    slices = [result[i:i+11] for i in range(0,len(result),11)]
-    print("morceaux : ",slices)
-    words = [word[:-1] for word in open("english.txt")]
-    mnemonic = ' '.join(words[int(m,2)] for m in slices)
-    print(mnemonic)
+    rng, hashe = generateEntropy() #Generate entropy with its sha256 hash
+    fullSeed = rng + hashe[:4] #Adding checksum (last 4 bits of hash) to the entropy
+    print(fullSeed)    
+    print(hex(int(fullSeed,2)))
+
+
+
+    slices = [fullSeed[i:i+11] for i in range(0,len(fullSeed),11)] #Slicing the seed in 11-bits sized slices (total of 12 slices)
+
+    words = [word[:-1] for word in open("english.txt")] #Importing the mnemonic english dictionnary (provided by BTC github)
+
+    mnemonic = ' '.join(words[int(m,2)] for m in slices) #Getting the corresponding mnemonic word to each slice of the previously sliced seed
+
+    return mnemonic
 
 def menu(): 
     terminated = False
@@ -27,4 +32,4 @@ def menu():
 
         input("Votre choix (chiffre) : ",end="")
 
-generateSeed()    
+print(generateSeed())
